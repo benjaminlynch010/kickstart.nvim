c7227b1fbac83d5730fb669b52bdd51ab2a28901
Ben Lynch <ben.lynch@kognitivedge.com>
2026-06-10 12:16:29 -0500

    vtsls config so i can use the Effect TS lsp

--- init.lua
+++ init.lua
@@ -696,13 +696,31 @@ require('lazy').setup({
       -- when it hits cross-workspace resolution issues). WARN captures real
       -- problems without the noise. Change to "ERROR" for even less output,
       -- or "DEBUG" temporarily when diagnosing LSP issues.
-      vim.lsp.set_log_level 'WARN'
+      vim.lsp.log.set_level 'WARN'
 
-      -- LSP servers and clients are able to communicate to each other what features they support.
-      --  By default, Neovim doesn't support everything that is in the LSP specification.
-      --  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
-      --  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
+	      -- LSP servers and clients are able to communicate to each other what features they support.
+	      --  By default, Neovim doesn't support everything that is in the LSP specification.
+	      --  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
+	      --  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
+	      local capabilities = require('blink.cmp').get_lsp_capabilities()
+	      capabilities.general = capabilities.general or {}
+	      capabilities.general.positionEncodings = { 'utf-16' }
+
+	      local vtsls_server = {
+	        settings = {
+	          vtsls = {
+	            autoUseWorkspaceTsdk = true,
+	          },
+	          typescript = {
+	            tsserver = {
+	              pluginPaths = { './node_modules' },
+	            },
+	          },
+	        },
+	      }
+	      vtsls_server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, vtsls_server.capabilities or {})
+	      vim.lsp.config('vtsls', vtsls_server)
+	      vim.lsp.enable('vtsls')
 
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -713,22 +731,15 @@ require('lazy').setup({
       --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-      local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer is configured explicitly below rather than left to Mason's
-        -- auto-handler, so that we can scope it to avoid indexing the entire monorepo.
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
+		local servers = {
+		  -- clangd = {},
+		  -- gopls = {},
+		  -- pyright = {},
+		  -- rust_analyzer is configured explicitly below rather than left to Mason's
+		  -- auto-handler, so that we can scope it to avoid indexing the entire monorepo.
+		  -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 
-        rust_analyzer = {
+		  rust_analyzer = {
           settings = {
             ['rust-analyzer'] = {
               files = {
@@ -785,23 +796,33 @@ require('lazy').setup({
       --
       -- You can add other tools here that you want Mason to install
       -- for you, so that they are available from within Neovim.
-      local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
-      })
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+		local ensure_installed = vim.tbl_keys(servers or {})
+		vim.list_extend(ensure_installed, {
+		  'vtsls',
+		  'stylua', -- Used to format Lua code
+		})
+		require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
-      require('mason-lspconfig').setup {
-        ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-        automatic_installation = false,
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
+		local disabled_servers = {
+		  vtsls = true,
+		  ts_ls = true,
+		}
+
+		require('mason-lspconfig').setup {
+		  ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
+		  automatic_installation = false,
+		  handlers = {
+		    function(server_name)
+		      if disabled_servers[server_name] then
+		        return
+		      end
+		      local server = servers[server_name] or {}
+		      -- This handles overriding only values explicitly passed
             -- by the server configuration above. Useful when disabling
             -- certain features of an LSP (for example, turning off formatting for ts_ls)
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
+		      vim.lsp.config(server_name, server)
+		      vim.lsp.enable(server_name)
           end,
         },
       }
@@ -953,7 +974,7 @@ require('lazy').setup({
     name = 'rose-pine',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
-      require('rose-pine').setup({
+      require('rose-pine').setup {
         variant = 'auto', -- auto, main, moon, or dawn
         dark_variant = 'main', -- main, moon, or dawn
         dim_inactive_windows = false,
@@ -1028,7 +1049,7 @@ require('lazy').setup({
           --     highlight.fg = palette.foam
           -- end
         end,
-      })
+      }
 
       vim.cmd.colorscheme 'rose-pine'
     end,
@@ -1185,7 +1206,7 @@ require('lazy').setup({
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
         additional_vim_regex_highlighting = { 'ruby' },
       },
-      indent = { enable = true, disable = { 'ruby' } },
+      indent = { enable = false, disable = { 'ruby' } },
     },
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
